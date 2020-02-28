7cc2399b806dd651d78428c5c24580ec8d3182f4
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-27 22:10:18 -0700

    Establish a few routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  get 'profile', to: 'welcome#profile'
+  get 'about/main', to: 'about#main'
+
 end
